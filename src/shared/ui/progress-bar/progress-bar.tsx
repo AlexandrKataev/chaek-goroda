9bcf22f638a45f7bdda3b006e46c0702c68362ad
AlexandrKataev <dkeyman@mail.ru>
2023-11-19 13:55:25 +0300

    change animation

--- src/shared/ui/progress-bar/progress-bar.tsx
+++ src/shared/ui/progress-bar/progress-bar.tsx
@@ -6,7 +6,7 @@ export const ProgressBar = (props: { progress: number; isPlayersTurn: boolean })
           width: `${props.progress}%`,
           height: '3px',
           backgroundColor: props.isPlayersTurn ? '#C4B5FD ' : '#DCDCDC',
-          transition: props.progress !== 100 ? 'width 1s ease-in-out' : 'none',
+          transition: props.progress !== 100 ? 'width 2s linear' : 'none',
         }}
       />
     </div>
